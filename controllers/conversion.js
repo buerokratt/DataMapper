09e24536267c38e32d2d52ccf9ab1dd1d377e4f8
Igor Krupenja <igor.krupenja@gmail.com>
2025-08-06 15:15:41 +0300

    fix(198): Fix Sonar

--- controllers/conversion.js
+++ controllers/conversion.js
@@ -10,13 +10,16 @@ import base64ToText from '../js/util/base64ToText.js';
 
 const router = express.Router();
 
-router.post('/csv_to_json', multer().array('file'), (req, res) => {
+// Common file size limit constant (5MB)
+const FILE_SIZE_LIMIT = 5 * 1024 * 1024;
+
+router.post('/csv_to_json', multer({ limits: { fileSize: FILE_SIZE_LIMIT } }).array('file'), (req, res) => {
   const file = base64ToText(req.body.file);
   const result = Papa.parse(file, { skipEmptyLines: true });
   res.json(result.data);
 });
 
-router.post('/yaml_to_json', multer().array('file'), (req, res) => {
+router.post('/yaml_to_json', multer({ limits: { fileSize: FILE_SIZE_LIMIT } }).array('file'), (req, res) => {
   const file = base64ToText(req.body.file);
   const result = parse(file);
   res.json(result);
